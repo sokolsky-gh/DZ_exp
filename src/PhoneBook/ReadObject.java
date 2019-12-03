4843cc907e59835d081326273c42a9a177d38292
unknown <m.sokolskiy@getqa.ru>
2019-12-03 06:56:23 +0300

    Ex1

---
+++ src/PhoneBook/ReadObject.java
@@ -0,0 +1,27 @@
+package PhoneBook;
+
+
+import java.io.*;
+import java.util.Arrays;
+
+public class ReadObject {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("people.bin");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            Person[] people = (Person[]) ois.readObject();
+
+            System.out.println(Arrays.toString(people));
+
+            ois.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
---
+++ src/PhoneBook/WriteObject.java
@@ -0,0 +1,23 @@
+package PhoneBook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class WriteObject {
+    public static void main(String[] args) {
+        Person[] people = {new Person(1, "Bob"),
+                new Person(2, "Mike"), new Person(3, "Tom")};
+
+        try {
+            FileOutputStream fos = new FileOutputStream("people.bin");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(people);
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
